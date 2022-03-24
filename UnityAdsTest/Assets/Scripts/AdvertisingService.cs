7c611ea1ad9c7db4dedeb4a62ef41227e81c40b0
Сергей <sergzv22@rambler.ru>
2022-03-24 14:20:07 +0400

    banner_bug_fix

--- UnityAdsTest/Assets/Scripts/AdvertisingService.cs
+++ UnityAdsTest/Assets/Scripts/AdvertisingService.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
 public class AdvertisingService : MonoBehaviour, IUnityAdsInitializationListener
 {
     [SerializeField] private bool testMode;
+
+    public Action InitializationComplete = delegate {};
     
     private string _androidGameId = "4673085";
     private string _iosGameId = "4673084";
@@ -17,12 +20,13 @@ public class AdvertisingService : MonoBehaviour, IUnityAdsInitializationListener
     private void InitializeAds()
     {
         _gameId = (Application.platform == RuntimePlatform.IPhonePlayer) ? _iosGameId : _androidGameId;
-        Advertisement.Initialize(_gameId, testMode);
+        Advertisement.Initialize(_gameId, testMode, this);
     }
     
     public void OnInitializationComplete()
-    {
+    { 
         Debug.Log("Ad initialization successful");
+        InitializationComplete.Invoke();
     }
 
     public void OnInitializationFailed(UnityAdsInitializationError error, string message)
--- UnityAdsTest/Assets/Scripts/BannerButton.cs
+++ UnityAdsTest/Assets/Scripts/BannerButton.cs
@@ -1,12 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.UI;
 
 public class BannerButton : MonoBehaviour
 {
+    [SerializeField] private AdvertisingService adService;
     [SerializeField] private BannerPosition bannerPosition = BannerPosition.TOP_CENTER;
     [SerializeField] private Button showBannerButton;
-
+    
     private string _androidAdUnitId = "Banner_Android";
     private string _iosAdUnitId = "Banner_iOS";
     private string _adUnitId;
@@ -15,16 +17,27 @@ public class BannerButton : MonoBehaviour
     {
         _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer) ? _iosAdUnitId : _androidAdUnitId;
         showBannerButton.interactable = false;
+        Advertisement.Banner.SetPosition(bannerPosition);
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        Advertisement.Banner.SetPosition(bannerPosition);
+        adService.InitializationComplete += OnInitializationComplete;
+    }
+
+    private void OnDisable()
+    {
+        adService.InitializationComplete -= OnInitializationComplete;
+    }
+
+    private void OnInitializationComplete()
+    {
         LoadAd();
     }
 
     private void LoadAd()
     {
+        Debug.Log("Uploading banner ads");
         BannerLoadOptions options = new BannerLoadOptions
         {
             loadCallback = OnBannerLoaded,
@@ -43,8 +56,7 @@ public class BannerButton : MonoBehaviour
 
     private void OnBannerError(string message)
     {
-        Debug.Log($"Banner Error: {message}, load another banner");
-        LoadAd();
+        Debug.LogError($"Banner Error: {message}");
     }
     
     private void ShowBannerAd()
